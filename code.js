70397c6e2437d20dcea7dd91f323658fdcf816b4
jkerr89115 <jkerr89115@uat.edu>
2024-08-04 18:35:21 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,24 +1,24 @@
-function play()
+function play()//defines the function through JavaScript for play()
 {
-    var die1 = Math.ceil(Math.random() * 6);
-    var die2 = Math.ceil(Math.random() * 6);
+    var die1 = Math.ceil(Math.random() * 6);//gives die 1 value
+    var die2 = Math.ceil(Math.random() * 6);//gives die 2 value
 
-    document.getElementById("die1res").innerHTML = "Die 1 =" + die1;
-    document.getElementById("die2res").innerHTML = "Die 2 =" + die2;
+    document.getElementById("die1res").innerHTML = "Die 1 =" + die1;//links JS ID to HTML ID
+    document.getElementById("die2res").innerHTML = "Die 2 =" + die2;//links JS ID to HTML ID
 
     var sum = die1 + die2;
-    document.getElementById("sumres").innerHTML = "Sum = " + sum;
+    document.getElementById("sumres").innerHTML = "Sum = " + sum;//links JS ID to HTML ID
 
-    if (sum ==7 || sum ==11)  // || = or
+    if (sum ==7 || sum ==11)//defines rules for outcome of function
     {
-    document.getElementById("finalres").innerHTML = "CRAPS YOU LOSE!!!";
+    document.getElementById("finalres").innerHTML = "CRAPS YOU LOSE!!!";//links JS ID to HTML ID
     }
-    else if (die1 == die2 && die1 % 2 == 0) //&& = and, x %2 ==0 means even
+    else if (die1 == die2 && die1 % 2 == 0) //defines rules for outcome of function
     {
-    document.getElementById("finalres").innerHTML = "Even Doubles - YOU WIN!!!";
+    document.getElementById("finalres").innerHTML = "Even Doubles - YOU WIN!!!";//links JS ID to HTML ID
     }
     else
     {
-    document.getElementById("finalres").innerHTML = "Push";
+    document.getElementById("finalres").innerHTML = "Push";//links JS ID to HTML ID
     }
 }
